d256271cc3a799e58bdeac7fccebc7a3f14dc3ba
elibiz443 <ellyambet10@gmail.com>
2024-05-22 02:39:16 +0300

    added login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,34 @@
 class SessionsController < ApplicationController
-  def index
+  skip_before_action :authorized!, only: [:index, :new, :create]
+
+  def index; end
+
+  def new; end
+
+  def create
+    reset_session
+    @user = User.find_by(username: session_params[:username])
+
+    respond_to do |format|
+      if @user && @user.authenticate(session_params[:password])
+        log_in(@user)
+        format.html { 
+          redirect_to '/', notice: "Welcome Back #{current_user.first_name} #{current_user.last_name}"
+        }
+      else
+        format.html { redirect_to '/sessions/new', notice: "Wrong Email & Password Confirmation. Plz Try Again!" }
+      end
+    end
   end
 
-  def new
+  def destroy
+    session.delete(:user_id)
+    redirect_to '/login', notice: 'Logged out!'
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:username, :password)
   end
 end
