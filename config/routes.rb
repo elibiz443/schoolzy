a132014747ee4cba94b40bc31f3ba47be8570555
elibiz443 <ellyambet10@gmail.com>
2024-05-22 02:31:57 +0300

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/show'
-  get 'users/index'
-  get 'users/new'
-  get 'sessions/index'
-  get 'sessions/new'
   get 'dashboard/index'
+
+  get 'login', to: 'sessions#index'
+  get "logout", to: "sessions#destroy"
+  get 'signup', to: 'users#new'
+  resources :users, only: [:create, :update, :destroy]
+  resources :sessions, only: [:new]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
