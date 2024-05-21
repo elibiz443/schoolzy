ac4a441298853c6e9d36cd879beb2f782e4fc06d
elibiz443 <ellyambet10@gmail.com>
2024-05-22 02:19:47 +0300

    Remove seeds.rb from repository

--- db/seeds.rb
+++
@@ -1,5 +0,0 @@
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
-User.create!(username: "elibiz443", first_name: "Eli", last_name: "Biz", role: "super_admin", password: "Security0443", password_confirmation: "Security0443")
